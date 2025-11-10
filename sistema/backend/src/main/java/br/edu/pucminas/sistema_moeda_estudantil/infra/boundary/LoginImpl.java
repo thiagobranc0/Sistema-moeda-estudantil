eb888a98df0e614c7a67664bee39ea142e60c73e
thiagobranc0 <thiagobranco48@yahoo.com>
2025-11-10 19:46:41 -0300

    feat: login

---
+++ sistema/backend/src/main/java/br/edu/pucminas/sistema_moeda_estudantil/infra/boundary/LoginImpl.java
@@ -0,0 +1,51 @@
+package br.edu.pucminas.sistema_moeda_estudantil.infra.boundary;
+
+import br.edu.pucminas.sistema_moeda_estudantil.infra.enums.TipoUsuario;
+import br.edu.pucminas.sistema_moeda_estudantil.infra.mapper.UsuarioMapper;
+import br.edu.pucminas.sistema_moeda_estudantil.model.domain.boundary.LoginBoundary;
+import br.edu.pucminas.sistema_moeda_estudantil.model.domain.dto.LoginDTO;
+import br.edu.pucminas.sistema_moeda_estudantil.model.domain.dto.UsuarioUpdateDTO;
+import br.edu.pucminas.sistema_moeda_estudantil.model.repository.AlunoRepository;
+import br.edu.pucminas.sistema_moeda_estudantil.model.repository.EmpresaRepository;
+import br.edu.pucminas.sistema_moeda_estudantil.model.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginImpl implements LoginBoundary {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private UsuarioMapper usuarioMapper;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
+    @Override
+    public UsuarioUpdateDTO login(LoginDTO loginDTO) {
+        var usuario = usuarioRepository.findByEmail(loginDTO.getEmail());
+
+        var id = usuario.getId();
+
+        UsuarioUpdateDTO usuarioUpdateDTO = usuarioMapper.toUsuarioUpdateDTO(usuario);
+
+        if (usuario.getTipo() == TipoUsuario.ALUNO) {
+            alunoRepository.findById(id).ifPresent(aluno -> {
+                usuarioUpdateDTO.setCpf(aluno.getCpf());
+                usuarioUpdateDTO.setRg(aluno.getRg());
+                usuarioUpdateDTO.setEndereco(aluno.getEndereco());
+            });
+        } else if (usuario.getTipo() == TipoUsuario.EMPRESA) {
+            empresaRepository.findById(id).ifPresent(empresa -> {
+                usuarioUpdateDTO.setCnpj(empresa.getCnpj());
+            });
+        }
+        return usuarioUpdateDTO;
+
+    }
+}
---
+++ sistema/backend/src/main/java/br/edu/pucminas/sistema_moeda_estudantil/infra/mapper/LoginMapper.java
@@ -0,0 +1,10 @@
+package br.edu.pucminas.sistema_moeda_estudantil.infra.mapper;
+
+import br.edu.pucminas.sistema_moeda_estudantil.model.LoginRequest;
+import br.edu.pucminas.sistema_moeda_estudantil.model.domain.dto.LoginDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface LoginMapper {
+    LoginDTO loginRequestToLoginDTO(LoginRequest loginRequest);
+}
